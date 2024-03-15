2ad705ebfcaf69cd1c1450627f30ea4fa73ffd95
Harshika jain <163335317+Harshika-jain@users.noreply.github.com>
2024-03-15 20:47:25 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class fact
+{
+  public static void main(string args[])
+{
+  int fact=1;
+scanner sc=new scanner(system.in);
+system.out.printin("enter the number");
+int n=sc.nextint();
+for(int i=1;i<=n;i++)
+{
+  fact=fact*i;
+}
+ system.out.printin("factorial* + facti);
+}
+}
+
